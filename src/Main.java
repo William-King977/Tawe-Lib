c3935392ec7325dfbebb5f9c1895f1553abddbcb
William King <williamking9772@gmail.com>
2020-07-08 18:56:17 +0100

    Added Main.java
    Initialised the Java project in eclipse IDE.

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * The main class starts the program.
+ * @author William King
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// Run the program
+		// System.out.println("Big Z");
+		// saaa
+
+	}
+
+}
